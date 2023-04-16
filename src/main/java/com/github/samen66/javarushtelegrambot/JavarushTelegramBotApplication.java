19a519498ff9986d219146804f2428a74d84f5c4
SAmen <samenuatkhan@gmail.com>
2023-04-17 05:34:44 +0600

    JRTB - 0 step-1 add spring boot

---
+++ src/main/java/com/github/samen66/javarushtelegrambot/JavarushTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.samen66.javarushtelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavarushTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavarushTelegramBotApplication.class, args);
+	}
+
+}
